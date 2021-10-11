972ca4ca7c08087bd8bc779f1f7b3ef65ee323ed
Kutay Can Kaynak <ktykynk814@gmail.com>
2021-10-11 17:30:49 +0300

    Final iteration

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,14 +6,29 @@ public class Ball : MonoBehaviour
 {
   public Rigidbody rb;
   public float jumpForce;
+  public GameObject splashPrefab;
+  private GameManager gm;
     
 
     // Update is called once per frame
     void Update()
     {
-        
+        gm = GameObject.FindObjectOfType<GameManager>();
     }
     private void OnCollisionEnter(Collision other) {
         rb.AddForce(Vector3.up * jumpForce);
+        GameObject splash = Instantiate(splashPrefab, transform.position + new Vector3(0f, -0.2f, 0f), transform.rotation);
+        splash.transform.SetParent(other.gameObject.transform);
+        string materialName = other.gameObject.GetComponent<MeshRenderer>().material.name;
+        Debug.Log("Materyal Adı" + materialName);
+     
+        if (materialName == "Unsafe Colour (Instance)"){
+          gm.RestartGame();
+          // bölüm yeniden başlayacak
+          Debug.Log("Game Over");
+        }else if (materialName == "Last Ring (Instance)") {
+          // bir sonraki levele geç
+          Debug.Log("Sonraki Leves");
+        }
     }
 }
